42a488cf876c7675f19e25e190afd564147fa870
attackoncs <2710651793@qq.com>
2022-07-12 17:46:44 +0800

    add executor operator interface

--- src/main/executor.h
+++ src/main/executor.h
@@ -1,58 +1,123 @@
 #include "optimizer.h"
 
 namespace mydb {
-class BaseOperator {
- public:
-  virtual bool exec() = 0;
-  Plan* plan_;
-  BaseOperator* next_;
-};
+    class BaseOperator {
+    public:
+        BaseOperator(Plan* plan, BaseOperator* next) : plan_(plan), next_(next) {}
+        ~BaseOperator() {}
+        virtual bool exec() = 0;
 
-class CreateOperator : public BaseOperator {
- public:
-  bool exec() override;
-};
+        Plan* plan_;
+        BaseOperator* next_;
+    };
 
-class DropOperator : public BaseOperator {};
+    class CreateOperator : public BaseOperator {
+    public:
+        CreateOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~CreateOperator() {}
+        bool exec() override;
+    };
 
-class InsertOperator : public BaseOperator {};
+    class DropOperator : public BaseOperator {
+    public:
+        DropOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~DropOperator() {}
+        bool exec() override;
+    };
 
-class UpdateOperator : public BaseOperator {};
+    class InsertOperator : public BaseOperator {
+    public:
+        InsertOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~InsertOperator() {}
+        bool exec() override;
+    };
 
-class SelectOperator : public BaseOperator {};
+    class UpdateOperator : public BaseOperator {
+    public:
+        UpdateOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~UpdateOperator() {}
+        bool exec() override;
+    };
 
-class TrxOperator : public BaseOperator {};
+    class DeleteOperator : public BaseOperator {
+    public:
+        DeleteOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~DeleteOperator() {}
+        bool exec() override;
+    };
 
-class ShowOperator : public BaseOperator {
- public:
-  bool exec() override;
-};
+    class TrxOperator : public BaseOperator {
+    public:
+        TrxOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~TrxOperator() {}
+        bool exec() override;
+    };
 
-class DeleteOperator : public BaseOperator {};
+    class ShowOperator : public BaseOperator {
+    public:
+        ShowOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~ShowOperator() {}
+        bool exec() override;
+    };
 
-class ScanOperator : public BaseOperator {};
+    class SelectOperator : public BaseOperator {
+    public:
+        SelectOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~SelectOperator() {}
+        bool exec() override;
+    };
 
-class IndexScanOperator : ScanOperator {};
+    class IndexScanOperator : public BaseOperator {
+    public:
+        IndexScanOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~IndexScanOperator() {}
+        bool exec() override;
+    };
 
-class SeqScanOperator : ScanOperator {};
+    class SeqScanOperator : public BaseOperator {
+    public:
+        SeqScanOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~SeqScanOperator() {}
+        bool exec() override;
+    };
 
-class FilterOperator : BaseOperator {};
+    class FilterOperator : public BaseOperator {
+    public:
+        FilterOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~FilterOperator() {}
+        bool exec() override;
+    };
 
-class SortOperator : BaseOperator {};
+    class SortOperator : public BaseOperator {
+    public:
+        SortOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~SortOperator() {}
+        bool exec() override;
+    };
 
-class LimitOperator : BaseOperator {};
+    class LimitOperator : public BaseOperator {
+    public:
+        LimitOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~LimitOperator() {}
+        bool exec() override;
+    };
 
-class ProjectionOperator : BaseOperator {};
+    class ProjectionOperator : public BaseOperator {
+    public:
+        ProjectionOperator(Plan* plan, BaseOperator* next) : BaseOperator(plan, next) {}
+        ~ProjectionOperator() {}
+        bool exec() override;
+    };
 
-class Executor {
- public:
-  Executor(Plan* plan) : planTree_(plan) {}
-  ~Executor();
-  bool generateOperator();
+    class Executor {
+    public:
+        Executor(Plan* plan) : planTree_(plan) {}
+        ~Executor();
+        BaseOperator* generateOperator(Plan* Plan);
 
- private:
-  Plan* planTree_;
-  BaseOperator* opTree_;
-};
+    private:
+        Plan* planTree_;
+        BaseOperator* opTree_;
+    };
 
 }
