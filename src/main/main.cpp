3862d877bab9cf39e425dec04e23f884814f563a
attackoncs <2710651793@qq.com>
2022-07-12 10:44:19 +0800

    add create statement parser (metadata,parser and util)

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,26 @@
-#include "sql/SQLStatement.h"
-#include <iostream>
+#include "SQLParser.h"
+#include "SQLParserResult.h"
+#include "util/sqlhelper.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <cstdlib.h>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "# Welcome to MyDB DB!!!" << std::endl;
+    std::cout << "# Input your query in one line." << std::endl;
+    std::cout << "# Enter 'exit' to quit this program." << std::endl;
+    std::string cmd;
+    while (true)
+    {
+        std::getline(std::cin, cmd);
+        if (cmd == "exit")
+        {
+            break;
+        }
+    }
+
+    std::cout << "# Farewell~~~ " << std::endl;
     return 0;
 }
---
+++ src/main/parser.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "SQLParser.h"
+#include "SQLParserResult.h"
+#include "util/sqlhelper.h"
+
+using namespace hsql;
+
+namespace mydb {
+    class Parser {
+    public:
+        Parser();
+        ~Parser();
+
+        bool parseStatement(std::string query);
+
+    private:
+        bool checkStmtsMeta();
+
+        bool checkMeta(const SQLStatement* stmt);
+
+        bool checkSelectStmt(const SelectStatement* select_stmt);
+
+        bool checkInsertStmt(const InsertStatement* insert_stmt);
+
+        bool checkUpdateStmt(const UpdateStatement* update_stmt);
+
+        bool checkDeleteStmt(const DeleteStatement* delete_stmt);
+
+        bool checkCreateStmt(const CreateStatement* create_stmt);
+
+        bool checkDropStmt(const DropStatement* drop_stmt);
+
+        bool checkCreateIndexStmt(const CreateStatement* stmt);
+
+        bool checkCreateTableStmt(const CreateStatement* stmt);
+
+        bool checkTable(TableRef* table);
+
+        SQLParserResult* result_;
+    };
+}
